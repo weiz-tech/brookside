2948cf0dd7db3cdb42766381b828d153bac20d5f
wei zhang <mc.weiz@qq.com>
2019-06-15 00:25:49 +0800

    添加gitignore文件

--- src/py/utils/FileUtil.py
+++ src/py/utils/FileUtil.py
@@ -1,23 +1,21 @@
 #-*-coding:utf-8-*-
 import os
+'''
+本类用于收集操作文件相关的工具方法。
+'''
 
-#查找文件
-#path="F:\\教程\\测试"
-path="F:\\教程\\Java并发编程原理与实战"
-#os.listdir()方法，列出来所有文件
-#返回path指定的文件夹包含的文件或文件夹的名字的列表
+'''
+方法1：说明：将指定目录下的所有文件重命名（文件名的第三个字符位置处加个点号）
+'''
+#查找路径：
+path="F:\\mypath\\py_file"
+#os.listdir()方法，列出指定目录下所有文件的文件名，不包含子目录
 files=os.listdir(path)
-
-#主逻辑
-#对于批量的操作，使用FOR循环
 for f in files:
-    #调试代码的方法：关键地方打上print语句，判断这一步是不是执行成功
     print(f)
     if  f.endswith(".mp4"):
-        #f[0:2]+"."+f[2:]
         print("原来的文件名字是:{}".format(f))
-
-        #找到老的文件所在的位置
+        #找到原文件所在的位置
         old_file=os.path.join(path,f)
         print("old_file is {}".format(old_file))
         #指定新文件的位置，如果没有使用这个方法，则新文件名生成在本项目的目录中
@@ -29,17 +27,13 @@ for f in files:
 
 
 
-#import os,sys
-#F:\教程\Java并发编程原理与实战
+#其他常用方法一览：
 #path="F:\\教程\\Java并发编程原理与实战"
 # 切换到 对应 目录
 #os.chdir(path )
-
 #列出目录
 #print("目录为：%s"%os.listdir(os.getcwd()))
-
 #移除
 #os.remove("project30.jpg")
-
 #移除后的目录
 #print("移除后：%s"%os.listdir(os.getcwd()))

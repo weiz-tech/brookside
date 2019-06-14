28e57cb67f9ec4226b6f4b7bd0fc0e8cab0f731d
wei zhang <mc.weiz@qq.com>
2019-06-15 00:50:59 +0800

    添加说明

--- src/py/utils/FileUtil.py
+++ src/py/utils/FileUtil.py
@@ -1,28 +1,34 @@
 #-*-coding:utf-8-*-
+
 import os
+
 '''
+author:yirenyijiu
 本类用于收集操作文件相关的工具方法。
 '''
 
+
+
 '''
-方法1：说明：将指定目录下的所有文件重命名（文件名的第三个字符位置处加个点号）
+方法1：renameByAddStr2FixedPos
+说明：将指定目录下指定类型的所有文件重命名（并且在文件名的指定位置添加指定的字符，以重命名）
 '''
-#查找路径：
-path="F:\\mypath\\py_file"
-#os.listdir()方法，列出指定目录下所有文件的文件名，不包含子目录
-files=os.listdir(path)
-for f in files:
-    print(f)
-    if  f.endswith(".mp4"):
-        print("原来的文件名字是:{}".format(f))
-        #找到原文件所在的位置
-        old_file=os.path.join(path,f)
-        print("old_file is {}".format(old_file))
-        #指定新文件的位置，如果没有使用这个方法，则新文件名生成在本项目的目录中
-        new_file=os.path.join(path,f[0:2]+"."+f[2:])
-        print("File will be renamed as:{}".format(new_file))
-        os.rename(old_file,new_file)
-        print("修改后的文件名是:{}".format(f))
+def renameByAddStr2FixedPos(path,suffix,index,words):    
+    #os.listdir()方法，列出指定目录下所有文件的文件名，不包含子目录
+    files=os.listdir(path)
+    for f in files:
+        if f.endswith(suffix):
+            print("原文件名：{}".format(f))
+            old_file=os.path.join(path,f)
+            new_file=os.path.join(path,f[0:index-1]+words+f[index-1:])
+            print("文件重命名为：{}".format(new_file))
+            os.rename(old_file,new_file)
+        
+
+
+#测试：在文件夹的第三个字符前，插入一个点号，即原文件名：01foo.mp4, 新文件名：01.foo.mp4
+path="F:\\mypath\\video"
+renameByAddStr2FixedPos(path,"mp4",3,".")
 
 
 
